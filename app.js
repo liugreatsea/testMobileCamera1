9a322f6b84e51010ed530182915d3215a765805e
derek <liugreatsea@163.com>
2017-03-10 00:09:19 +0800

    add https port

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+// var https = require('https');
+
+var fs = require('fs');
+
 // create a new express server
 var app = express();
 
@@ -21,8 +25,29 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+// Determine port to listen on
+var port = (process.env.PORT || process.env.VCAP_APP_PORT || 6001);
+
+
+// // start server on the specified port and binding host
+var server = app.listen(port, function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+
+// /*For local https test only*/
+// var options = {
+//   key: fs.readFileSync('./privatekey.pem'),
+//   cert: fs.readFileSync('./certification.pem')
+// };
+//
+// var SSLPORT = 3011;
+//
+// var httpsServer = https.createServer(options, app);
+//
+//
+// httpsServer.listen(SSLPORT, function() {
+//   console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT);
+// });
+
